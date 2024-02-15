125fbfd6b0c4dff00cbaaf5becf5c155f5ac3bf7
Keval Panwala <107193741+kevalpanwala27@users.noreply.github.com>
2024-02-15 18:44:39 +0530

    create and run the server

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
