49e8914988795db96c552232bea8fd8c9131f439
Keval Panwala <107193741+kevalpanwala27@users.noreply.github.com>
2024-02-15 19:06:49 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB: ", err);
+  });
+
 const app = express();
 
 app.listen(3000, () => {
