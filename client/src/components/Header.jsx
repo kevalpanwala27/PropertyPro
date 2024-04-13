f2be249c394cf4bd1cdfedbba28e978d4212aa41
Keval Panwala <107193741+kevalpanwala27@users.noreply.github.com>
2024-04-13 13:30:18 +0530

    added build for deploy

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -27,8 +27,8 @@ export default function Header() {
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-500">Property</span>
-            <span className="text-slate-700">Pro</span>
+            <span className="text-slate-700">Property</span>
+            <span className="text-red-700">Pro</span>
           </h1>
         </Link>
         <form
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -50,7 +50,7 @@ export default function Home() {
       {/* top */}
       <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
         <h1 className="text-slate-700 font-bold text-3xl lg:text-6xl">
-          Find your next <span className="text-slate-500">perfect</span>
+          Find your next <span className="text-red-700">perfect</span>
           <br />
           place with ease
         </h1>
